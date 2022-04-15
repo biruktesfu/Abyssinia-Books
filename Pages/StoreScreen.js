967f59104301db943072ab2111bdacd44bf6f36f
BonsonDem <it.bonson.demssie@gmail.com>
2022-04-15 21:54:43 +0300

    searchbar added to library and store screen

--- Pages/StoreScreen.js
+++ Pages/StoreScreen.js
@@ -1,8 +1,21 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { SearchBar } from "@rneui/themed";
+import { Text, View, StyleSheet } from "react-native";
 
 function StoreScreen() {
+  const [search, setSearch] = useState("");
+
+  const updateSearch = (search) => {
+    setSearch(search);
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1, justifyContent: "flex-start", alignItems:"stretch" }}>
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={updateSearch}
+        value={search}
+      />
       <Text>Store</Text>
     </View>
   );
